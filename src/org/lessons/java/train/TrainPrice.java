8ee40a51fb1abd38f05e680b7066fa7971a2ded5
mcspe <mcspezzaferro@gmail.com>
2023-09-13 17:42:59 +0200

    [add] project

--- src/org/lessons/java/train/TrainPrice.java
+++ src/org/lessons/java/train/TrainPrice.java
@@ -1,10 +1,38 @@
 package org.lessons.java.train;
 
+import java.util.Scanner;
+
 public class TrainPrice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		float unitPrice = 0.21f; //prezzo unitario al km
+		float underageDiscount = 0.20f;
+		float overageDiscount = 0.40f;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("BENVENUTO SU TRAIN TICKET PRICE CALCULATOR");
+		
+		System.out.print("Inserisci i KM che intendi percorrere: ");
+		int km = sc.nextInt();
+		
+		System.out.print("Inserisci la tua età: ");
+		int age = sc.nextInt();
+		
+		sc.close();
+		
+		float price = km * unitPrice;
+		
+		if (age < 18) {
+			price *= (1 - underageDiscount);
+		} 
+		
+		if (age > 64) {
+			price *= (1 - overageDiscount);
+		}
+		
+		System.out.println("Il prezzo del tuo biglietto sarà di €" + String.format("%.02f", price));
 	}
 
 }
